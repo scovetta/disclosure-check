51effb78a6531498867973a4de977a527b6b98e6
Michael Scovetta <michael.scovetta@gmail.com>
2023-04-27 16:50:28 -0700

    Build Docker container along with dist

--- build-test.sh
+++ build-test.sh
@@ -10,3 +10,6 @@ python -mvenv ../venv1
 source ../venv1/bin/activate
 pip install dist/disclosurecheck-*.tar.gz
 disclosurecheck --help
+
+VERSION=$(cat pyproject.toml| grep version | cut -d\" -f2)
+docker build -t disclosurecheck:$VERSION .
